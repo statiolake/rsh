680db0c795590cb4e8990977a9bc166aa629b35a
statiolake <statiolake@gmail.com>
2018-05-12 15:03:01 +0900

    implement basic command invokation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,90 @@
+#![feature(box_syntax)]
+
+#[macro_use]
+extern crate colored_print;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+mod consts;
+mod expr;
+mod parser;
+
+use std::error;
+use std::fmt;
+use std::io;
+use std::io::prelude::*;
+use std::result;
+
+use colored_print::color::ConsoleColor;
+
+use parser::Parser;
+
+pub const COLOR_ERROR: ConsoleColor = ConsoleColor::Red;
+
+pub type Result<T> = result::Result<T, RshError>;
+
+pub trait ErrorChainToRshError<T> {
+    fn chain_err(self) -> Result<T>;
+}
+
+#[derive(Debug)]
+pub enum RshError {
+    ExprError(expr::ExprError),
+    ParseError(parser::ParseError),
+}
+
+impl fmt::Display for RshError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "{:?}", self)
+    }
+}
+
+impl<T> ErrorChainToRshError<T> for expr::Result<T> {
+    fn chain_err(self) -> Result<T> {
+        self.map_err(|e| RshError::ExprError(e))
+    }
+}
+
+impl<T> ErrorChainToRshError<T> for parser::Result<T> {
+    fn chain_err(self) -> Result<T> {
+        self.map_err(|e| RshError::ParseError(e))
+    }
+}
+
+impl error::Error for RshError {
+    fn cause(&self) -> Option<&dyn error::Error> {
+        match *self {
+            RshError::ExprError(ref e) => Some(e),
+            RshError::ParseError(ref e) => Some(e)
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let stdin = io::stdin();
+    let mut stdin = stdin.lock();
+    loop {
+        if let Err(e) = run_once(&mut stdin) {
+            colored_println! {
+                true;
+                COLOR_ERROR, "error: ";
+                ConsoleColor::Reset, "{}", e;
+            }
+        }
+    }
+}
+
+fn run_once(stdin: &mut io::StdinLock) -> Result<()> {
+    print!("> ");
+    io::stdout().flush().unwrap();
+    let mut line = String::new();
+    stdin.read_line(&mut line).unwrap();
+    let expr = Parser::from(line.trim()).parse().chain_err()?;
+    debug!("parser result: {:?}", expr);
+    let mut cmd = expr.make_toplevel_command().chain_err()?;
+    debug!("invoke cmd: {:?}", cmd);
+    println!("{:?}", cmd.output());
+    Ok(())
 }
