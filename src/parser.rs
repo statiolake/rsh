a89fd7b44d3148aa4d54acc3e93c0e337922261f
statiolake <statiolake@gmail.com>
2019-01-07 03:10:17 +0900

    rewrite parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,19 +1,19 @@
 use std::error;
 use std::fmt;
-use std::mem;
+use std::iter::Peekable;
 use std::result;
 use std::str::Chars;
+
+use itertools::Itertools;
 
 use crate::expr::Expr;
 
 pub const ESCAPE_CHAR: char = '`';
 
-#[derive(Clone)]
 struct State {
     double_quote: bool,
     single_quote: bool,
     escaped: bool,
-    just_after_whitespace: bool,
 }
 
 impl State {
@@ -22,7 +22,6 @@ impl State {
             double_quote: false,
             single_quote: false,
             escaped: false,
-            just_after_whitespace: false,
         }
     }
 
@@ -31,7 +30,6 @@ impl State {
             double_quote: self.double_quote,
             single_quote: self.single_quote,
             escaped: false,
-            just_after_whitespace: false,
         }
     }
 }
@@ -50,142 +48,120 @@ impl fmt::Display for ParseError {
 impl error::Error for ParseError {}
 
 impl ParseError {
+    #[allow(dead_code)]
     pub fn new(kind: ErrorKind) -> ParseError {
         ParseError { kind }
     }
 }
 
 #[derive(Debug)]
-pub enum ErrorKind {
-    OpenParenthesisIsNotJustAfterWhitespace,
-
-    DoubleQuoteNotEnded,
-    SingleQuoteNotEnded,
-}
+pub enum ErrorKind {}
 
 pub type Result<T> = result::Result<T, ParseError>;
 
 pub struct Parser<'a> {
-    chars: Chars<'a>,
+    chars: Peekable<Chars<'a>>,
 }
 
 impl<'a> From<&'a str> for Parser<'a> {
     fn from(s: &str) -> Parser {
-        Parser { chars: s.chars() }
+        Parser::new(s.chars().peekable())
     }
 }
 
 impl<'a> Parser<'a> {
-    pub fn new(chars: Chars<'a>) -> Parser {
+    pub fn new(chars: Peekable<Chars<'a>>) -> Parser {
         Parser { chars }
     }
 
     pub fn parse(&mut self) -> Result<Expr> {
-        self.parse_inner().map(|x| x.0)
+        self.parse_fncall()
     }
 
-    fn parse_inner(&mut self) -> Result<(Expr, Chars<'a>)> {
-        let mut state_mut = State::new();
+    fn parse_inner(&mut self) -> Result<Expr> {
+        if let Some('(') = self.chars.peek() {
+            assert_eq!(self.chars.next(), Some('('), "logic error");
+            let expr = self.parse_fncall();
+            assert_eq!(self.chars.next(), Some(')'), "incorrect close delimiter");
+            expr
+        } else {
+            self.parse_literal()
+        }
+    }
+
+    fn parse_fncall(&mut self) -> Result<Expr> {
+        // drop delimiter whitespaces
+        fn drop_whitespace_chars(chars: &mut Peekable<Chars>) {
+            chars
+                .peeking_take_while(|&ch| Parser::is_whitespace(ch))
+                .for_each(drop);
+        }
 
         let mut expr = Vec::new();
-        let mut arg = String::new();
-        while let Some(ch) = self.chars.next() {
-            let state = state_mut;
-            let mut next_state = state.next();
-
-            let ch_is_whitespace = Parser::is_whitespace(ch);
+        while let Some(ch) = self.chars.peek() {
             match ch {
-                // when escaping current char
-                ch if state.escaped => {
-                    arg.push(ch);
-                }
-
-                // when whitespace in double/single-quoted string
-                ch if ch_is_whitespace && state.double_quote || state.single_quote => {
-                    arg.push(ch);
-                }
-
-                // other whitespace : delimiter
-                _ if ch_is_whitespace => {
-                    // if there's something in arg push it
-                    if !arg.is_empty() {
-                        expr.push(Expr::Literal(mem::replace(&mut arg, String::new())));
-                    }
-                    next_state.just_after_whitespace = true;
-                }
-
-                // when escape character
-                ESCAPE_CHAR => {
-                    next_state.escaped = true;
-                }
-
-                // when double-quote character in single-quoted string
-                '\"' if state.single_quote => {
-                    // treat it as ordinal character
-                    arg.push('\"');
-                }
-
-                // when double-quote character elsewhere
-                '\"' => {
-                    // beginning or ending of double-quoted string
-                    next_state.double_quote = !state.double_quote;
-                }
-
-                // when single-quote character in double-quoted string
-                '\'' if state.double_quote => {
-                    // treat it as ordinal character
-                    arg.push('\'');
-                }
-
-                // when single-quote character elsewhere
-                '\'' => {
-                    // beginning or ending of single-quoted string
-                    next_state.single_quote = !state.single_quote;
-                }
-
-                // when parenthesis in single/double-quoted string
-                ch @ '(' | ch @ ')' if state.single_quote || state.double_quote => arg.push(ch),
-
-                // when open-parenthesis elsewhere
-                '(' => {
-                    if !arg.is_empty() {
-                        return Err(ParseError::new(
-                            ErrorKind::OpenParenthesisIsNotJustAfterWhitespace,
-                        ));
-                    }
-                    let (child_expr, new_chars) = Parser::new(self.chars.clone()).parse_inner()?;
-                    self.chars = new_chars;
-                    expr.push(child_expr);
-                }
-
-                // when close-parenthesis
-                ')' => {
-                    // push last argument AFTER breaking; nothing do here
-                    state_mut = next_state;
-                    break;
-                }
-
-                // other characters
-                ch => {
-                    arg.push(ch);
+                ')' => break,
+                _ => {
+                    expr.push(self.parse_inner()?);
+                    drop_whitespace_chars(&mut self.chars);
                 }
             }
-            state_mut = next_state;
         }
 
-        if state_mut.double_quote {
-            return Err(ParseError::new(ErrorKind::DoubleQuoteNotEnded));
+        Ok(Expr::FnCall(expr))
+    }
+
+    fn parse_literal(&mut self) -> Result<Expr> {
+        let mut state = State::new();
+        let mut literal = String::new();
+        while let Some(&ch) = self.chars.peek() {
+            let whitespace = Parser::is_whitespace(ch);
+
+            let mut next_state = state.next();
+            match ch {
+                // when escaping current char
+                ch if state.escaped => literal.push(ch),
+
+                // when escape character
+                ESCAPE_CHAR => next_state.escaped = true,
+
+                // when double-quote character in single-quoted string --- treat it as ordinal
+                // character
+                '\"' if state.single_quote => literal.push('\"'),
+
+                // when double-quote character elsewhere --- beginning or ending of double-quoted
+                // string
+                '\"' => next_state.double_quote = !state.double_quote,
+
+                // when single-quote character in double-quoted string --- treat it as ordinal
+                // character
+                '\'' if state.double_quote => literal.push('\''),
+
+                // when single-quote character elsewhere --- beginning or ending of single-quoted
+                // string
+                '\'' => next_state.single_quote = !state.single_quote,
+
+                // other ordinal characters in quotation
+                ch if state.double_quote || state.single_quote => literal.push(ch),
+
+                // other whitespace : delimiter
+                _ if whitespace => break,
+
+                // close parenthesis : delimiter
+                ')' => break,
+
+                // other ordinal characters
+                ch => literal.push(ch),
+            }
+
+            // drop current char
+            let _ = self.chars.next();
+            state = next_state;
         }
 
-        if state_mut.single_quote {
-            return Err(ParseError::new(ErrorKind::SingleQuoteNotEnded));
-        }
-
-        if !arg.is_empty() {
-            expr.push(Expr::Literal(arg));
-        }
-
-        Ok((Expr::FnCall(expr), self.chars.clone()))
+        assert!(!state.double_quote, "double quote not ended.");
+        assert!(!state.single_quote, "single quote not ended.");
+        Ok(Expr::Literal(literal))
     }
 
     fn is_whitespace(ch: char) -> bool {
